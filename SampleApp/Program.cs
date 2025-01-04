e1530c0392160eaa40acbc83e13b23cfde2a6c73
Chris Missal <chris.missal@gmail.com>
2025-01-04 16:38:23 -0600

    adds d3 graph

--- SampleApp/Program.cs
+++ SampleApp/Program.cs
@@ -19,6 +19,13 @@ namespace SampleApp
             app.UseHttpsRedirection();
 
             app.UseMiddleware<GraphJsonMiddleware>(Assembly.GetExecutingAssembly());
+            // Serve the D3.js visualization HTML file
+            app.MapGet("/", async context =>
+            {
+                var htmlPath = Path.Combine(Directory.GetCurrentDirectory(), "graph.html");
+                context.Response.ContentType = "text/html";
+                await context.Response.SendFileAsync(htmlPath);
+            });
 
             app.Run();
         }
